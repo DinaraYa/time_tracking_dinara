4affcd8b1b64c0848a528b87d43c30127cced1ce
DinaraYa <tylemisovadinara@gmail.com>
2025-09-05 01:25:57 +0300

    hw_16

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
-import {sayHi} from "./tools.ts";
+import {launchServer} from "./server.ts"
+import * as mongoose from "mongoose";
+import {configuration} from "./config/libConfig.ts";
 
-const myName = "Konstantin";
-sayHi(myName);
-const x = "ghjfjkg"
+console.log("Connecting to MongoDB URI:", process.env.MONGO_URI);
+
+mongoose.connect(configuration.mongoUri)
+    .then(() => {
+        console.log("MongoDB successfully connected");
+        launchServer();
+    })
+    .catch((e) => {
+        console.log("Something went wrong", e);
+    });
