4179ebcfeda8ba017922e3580a5cc1e110f8bbfc
DinaraYa <tylemisovadinara@gmail.com>
2025-09-19 18:47:38 +0300

    hw_20: docs: update README.md with project description

--- src/Logger/winston.ts
+++ src/Logger/winston.ts
@@ -7,9 +7,9 @@ export const logger = winston.createLogger({
     format: winston.format.json(),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({filename: 'error.log', level: 'error'}),
-        new winston.transports.File({filename: 'combine.log'}),
-        new winston.transports.File({filename: 'warning.log', level: 'warn'}),
-        new winston.transports.File({filename: 'actions.log', level: 'info'})
+        new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
+        new winston.transports.File({filename: 'logs/combine.log'}),
+        new winston.transports.File({filename: 'logs/warning.log', level: 'warn'}),
+        new winston.transports.File({filename: 'logs/actions.log', level: 'info'})
     ]
 })
